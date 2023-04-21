0a961a2adbb223d8c5db7999bcb29726ba51371b
Raul A. Costa <113145571+Raulcjo@users.noreply.github.com>
2023-04-21 16:06:47 -0300

    add: Aula_12

---
+++ Aula12/listaluno.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lista Aluno</title>
+</head>
+<body>
+<?php
+    include 'conectabanco.php';
+    $nome = $_POST['nome'];
+    $endereco = $_POST['endereco'];
+    if($nome != "" and $endereco != ""){
+        //Escreve o comando
+        $sql = "INSERT INTO aluno (nome, endereco) 
+                VALUES ('$nome', '$endereco')";
+        //Executa o comando
+        $banco->query($sql);
+        if($banco->affected_rows >= 1){
+            echo "Aluno $nome cadastrado com sucesso!";
+        }else{
+            echo "Erro ao cadastrar Aluno $nome no banco de dados";
+        }
+    }
+?>
+
+<h1>listagem de Alunos</h1>
+<?php
+$select = "SELECT * FROM aluno";
+$resultado = mysqli_query($banco, $select);
+
+?>
+<table>
+    <thead>
+        <th>ID</th> <th>Nome</th> <th>Endereço</th> <th>Ação</th>
+    </thead>
+    <tbody>
+        <?php 
+        if($resultado){
+            while($linha = mysqli_fetch_assoc($resultado)){
+                $id_result = $linha['id'];
+                $nome_result = $linha['nome'];
+                $endereco_result = $linha['endereco'];
+                echo " <tr>
+                <td>$id_result</td> <td>$nome_result</td> <td>endereco_result</td> <td>??</td>
+                        </tr> ";
+                    }
+                }
+        ?>
+    </tbody>
+</table>
+
+    <?php
+    $banco->close();
+    ?>
+</body>
+</html>
